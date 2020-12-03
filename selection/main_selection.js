12e8e2146a9277c96f8fd7c17817d2af6df3c4d5
Rafael Moraes Barros <rafael_m_barros@hotmail.com>
2020-12-03 15:52:25 +0000

    update

---
+++ selection/main_selection.js
@@ -0,0 +1,17 @@
+
+const numbers = [5,5,5,10,20]
+
+numbers.reduce((a,b) => a+b)
+
+console.log(numbers.reduce((a,b) => a+b , 0))
+
+
+const people = [
+    {names: 'Rafa', age: 22 , heigth: 175,},
+    {names: 'Rod', age: 28, heigth: 176,}
+]
+
+
+
+console.log(`${people[0].names}`)
+
