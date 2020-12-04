9c6185511c908a9e4d214c1377f083eb1f4fea7b
Rafael Moraes Barros <rafael_m_barros@hotmail.com>
2020-12-04 01:05:28 +0000

    Update

---
+++ thirty projects/Drum kit/main_drum.js
@@ -0,0 +1,59 @@
+
+window.addEventListener('keydown',playSound)
+
+function playSound (e) {
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    const audioKey = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    if(!audioKey){
+        return
+    }else {
+        audioKey.currentTime = 0
+        audioKey.play()
+        key.classList.add('playing')
+        key.addEventListener('transitionend',removeClass)
+
+        function removeClass(e) {
+            
+        key.className = 'key'
+
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
