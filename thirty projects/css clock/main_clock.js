120d654334d9d89ab38b180a8ef48588502f07ef
Rafael Moraes Barros <rafael_m_barros@hotmail.com>
2020-12-06 00:49:06 +0000

    update

--- thirty projects/css clock/main_clock.js
+++ thirty projects/css clock/main_clock.js
@@ -12,51 +12,20 @@ setInterval(
     const handSecond = document.querySelector('.second')
     
     
-    if(hour === 0 || min === 0){
-        handHour.style.transform = `rotate(90deg)`
-        handMin.style.transform = `rotate(90deg)`
-    }else {
-        handHour.style.transform = `rotate(${(((hour)*360)/60)+90}deg)`
-        handMin.style.transform = `rotate(${(((min)*360)/60)+90}deg)`
-    }
+    hour === 0 ? handHour.style.transform = `rotate(90deg)` : handHour.style.transform = `rotate(${(((hour)*360)/12)+90}deg)`
     
+    min === 0 ? handMin.style.transform = `rotate(90deg)` : handMin.style.transform = `rotate(${(((min)*360)/60)+90}deg)`
+
     
     handSecond.style.transform = `rotate(${(((seconds)*360)/60)+90}deg)`
-   
+    
     console.log(hour)
     console.log(min)
     
-
-
+    
+    
 }
 , 1000); 
-
-// console.log(newDate())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
