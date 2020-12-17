819f34bc9273f37b60571d49229e14a3a4561b3b
Rafael Moraes Barros <rafael_m_barros@hotmail.com>
2020-12-17 17:28:02 +0000

    update

--- promises/mains.js
+++ promises/mains.js
@@ -41,16 +41,24 @@ makeAPizza(['pepperoni'])
         console.log(pizza)
     })
 
+    const pizza1 = makeAPizza(['ham', 'cheese', 'eggs', 'mushroms','pineapple', 'chicken','onions'])
+
+    const pizza2 = makeAPizza(['pineapple', 'chicken','onions'])
+
+    const pizza3 = makeAPizza(['ham', 'cheese', 'eggs', 'mushroms','pineapple'])
 
 
-
-
-
-
-
-
-
-
+    const dinner = Promise.all([pizza1, pizza2, pizza3])
+    .then(function(complex, simple, normal)
+    {
+        console.log(complex, simple, normal)
+    })
+   
+    const hungry = Promise.race([pizza1, pizza2, pizza3])
+    .then (function (complex){
+        console.log('Here is the first one!')
+        console.log(complex)
+    })
 
 
 
