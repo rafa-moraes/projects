facdfb1e592a20dc9199f8b2ac6ccb4d940f6d19
Rafael Moraes Barros <rafael_m_barros@hotmail.com>
2021-01-07 22:15:40 +0000

    update

--- canvas draw/mains.js
+++ canvas draw/mains.js
@@ -1,4 +1,13 @@
-
+const range = document.querySelector('#rangeInput')
+let lineCanvas = 10
+let isRange = false
+range.addEventListener('mousedown', ()=> isRange = true )
+range.addEventListener('mouseup', ()=> isRange = false )
+range.addEventListener('mousemove', rangeWidth)
+function rangeWidth (e) {
+    if(!isRange) return
+lineCanvas = this.value
+}
 const canvas = document.querySelector('.canvas')
 const ctx = canvas.getContext('2d')
 //make the width and height the size of the screen
@@ -7,6 +16,9 @@ canvas.height = window.innerHeight
 ctx.strokeStyle = 'yellow'
 ctx.lineJoin = 'round'
 ctx.lineCap = 'round'
+// make the color to blend 
+// ctx.globalCompositeOperation = 'multiply'
+let hue = 0 
 
 let isDrawing = false
 let lastX = 0
@@ -16,6 +28,8 @@ function draw (e) {
 if (!isDrawing) {
     return
 }
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
+ctx.lineWidth = lineCanvas
 ctx.beginPath()
 // start from
 ctx.moveTo(lastX, lastY)
@@ -24,11 +38,21 @@ ctx.lineTo(e.offsetX, e.offsetY)
 ctx.stroke()
 lastX = e.offsetX
 lastY = e.offsetY
+hue++
+if(hue >= 360){
+    hue = 0
 }
+// lineCanvas++
+// if(lineCanvas >=80){
+//     lineCanvas = 10
+// }
+}
+
 canvas.addEventListener('mousedown', (e) => {
 isDrawing = true
 lastX = e.offsetX
 lastY = e.offsetY
+
 })
 
 canvas.addEventListener('mousemove', draw)
