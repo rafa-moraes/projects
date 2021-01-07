b6ed577410d04d4e6aeff1542b977293b2817733
Rafael Moraes Barros <rafael_m_barros@hotmail.com>
2021-01-07 21:25:38 +0000

    update

--- canvas draw/mains.js
+++ canvas draw/mains.js
@@ -9,6 +9,38 @@ ctx.lineJoin = 'round'
 ctx.lineCap = 'round'
 
 let isDrawing = false
+let lastX = 0
+let lastY = 0
+
+function draw (e) {
+if (!isDrawing) {
+    return
+}
+ctx.beginPath()
+// start from
+ctx.moveTo(lastX, lastY)
+// go to
+ctx.lineTo(e.offsetX, e.offsetY)
+ctx.stroke()
+lastX = e.offsetX
+lastY = e.offsetY
+}
+canvas.addEventListener('mousedown', (e) => {
+isDrawing = true
+lastX = e.offsetX
+lastY = e.offsetY
+})
+
+canvas.addEventListener('mousemove', draw)
+canvas.addEventListener('mouseup', () => isDrawing = false)
+canvas.addEventListener('mouseout', () => isDrawing = false)
+
+
+
+
+
+
+
 
 
 
