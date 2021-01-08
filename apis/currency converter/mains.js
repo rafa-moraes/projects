ce63b52338e37950dc5526df0380e29c4b0eb545
Rafael Moraes Barros <rafael_m_barros@hotmail.com>
2021-01-08 19:37:20 +0000

    Update

---
+++ apis/currency converter/mains.js
@@ -0,0 +1,77 @@
+const fromSelect = document.querySelector('[name="from_currency"]')
+const toSelect = document.querySelector('[name="to_currency"]')
+const endPoint = 'https://api.exchangeratesapi.io/latest'
+const ratesByBase ={}
+
+const currencies = {
+    AUD:'Australian Dollar',
+    BGN:'Bulgarian Lev',
+    BRL:'Brazilian Real',
+    CAD:'Canadian Dollar',
+    CHF:'Swiss Franc',
+    CNY:'Chinese Yuan',
+    CZK:'Czech Republic Koruna',
+    DKK:'Danish Krone',
+    EUR: 'Euro',
+    GBP:'British Pound Sterling',
+    HKD:'Hong Kong Dollar',
+    HRK:'Croatian Kuna',
+    HUF:'Hungarian Forint',
+    IDR:'Indonesian Rupiah',
+    ILS:'Israeli New Sheqel',
+    INR:'Indian Rupee',
+    JPY:'Japanese Yen',
+    KRW:'South Korean Won',
+    MXN:'Mexican Peso',
+    MYR:'Malaysian Ringgit',
+    NOK:'Norwegian Krone',
+    NZD:'New Zeland Dolar',
+    PHP:'Philippine Peso',
+    PLN:'Polish Zloty',
+    RON:'Romanian Leu',
+    RUB:'Russian Ruble',
+    SEK:'Swedish Krona',
+    SGD:'Singapore Dolar',
+    THB:'Thai Baht',
+    TRY:'Turkish Libra',
+    USD:'United States Dollar',
+    ZAR:'South African Rand',
+
+}
+
+function generateOptions (options){
+    return Object.entries(options).map(([currencyCode, currencyName]) =>
+        `<option value ="${currencyCode}">${currencyCode} - ${currencyName}</option>`
+    ).join("")
+    
+
+}
+const optionsHTML = generateOptions(currencies)
+
+fromSelect.innerHTML = optionsHTML
+toSelect.innerHTML = optionsHTML
+
+async function fetchRates (base ='USD') {
+ const res = await fetch(`${endPoint}?base=${base}`)
+ const rates = await res.json()
+ return rates
+}
+
+async function convert (amount, from, to){
+// check if we even have the rates to convert from that currency
+if(!ratesByBase[from]){
+    const rates = await fetchRates(from)
+    ratesByBase[from] = rates
+}
+//  convert the amount they passed in
+const rate = ratesByBase[from].rates[to]
+const convertedAmount = rate * amount
+console.log(convertedAmount)
+return convertedAmount
+
+
+}
+
+
+
+
