faeee00b1f7e7ff0f852ee82eb30ea2da4b9c584
Rafael Moraes Barros <rafael_m_barros@hotmail.com>
2021-03-23 20:35:38 +0000

    Update main.js

--- problem 2/main.js
+++ problem 2/main.js
@@ -1,33 +1,33 @@
-// Test: gcdIter(99, 144)
-// Output:
-// 9
-// Test: gcdIter(68, 17)
-// Output:
-// 17
-// Test: gcdIter(20, 20)
-// Output:
-// 20
-// Test: gcdIter(154, 242)
-// Output:
-// 22
-// Test: gcdIter(10, 14)
-// Output:
-// 2
-// Test: gcdIter(3, 25)
-// Output:
-// 1
-// Test: gcdIter(26, 40)
-// Output:
-// 2
-// Test: gcdIter(175, 91)
-// Output:
-// 7
-// Test: gcdIter(69, 66)
-// Output:
-// 3
-// Test: gcdIter(360, 288)
-// Output:
-// 72
+Test: gcdIter(99, 144)
+Output:
+9
+Test: gcdIter(68, 17)
+Output:
+17
+Test: gcdIter(20, 20)
+Output:
+20
+Test: gcdIter(154, 242)
+Output:
+22
+Test: gcdIter(10, 14)
+Output:
+2
+Test: gcdIter(3, 25)
+Output:
+1
+Test: gcdIter(26, 40)
+Output:
+2
+Test: gcdIter(175, 91)
+Output:
+7
+Test: gcdIter(69, 66)
+Output:
+3
+Test: gcdIter(360, 288)
+Output:
+72
 
 // Test: gcdRecur(125, 90)
 // Output:
